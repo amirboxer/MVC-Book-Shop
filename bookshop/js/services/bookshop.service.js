01024633b67cc658dfbf0e4007632fdb2571bc22
amirboxer <amirboxer95@gmail.com>
2024-06-01 16:00:02 +0200

    Create Basic initial structure

---
+++ bookshop/js/services/bookshop.service.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// Basic model
+//{
+//    id: bg4J78,
+//    title: 'The adventures of Lori Ipsi',
+//    price: 120,
+//    imgUrl: 'lori-ipsi.jpg'
+//    }
+//    1
+    
+'use strict'
+
+var gBooks = [
+    { id: 'df45ef', title: 'The Advantures of lori Ipsi', price: 120, imgUrl: null},
+    { id: 'dj45kd', title: 'World Atlas', price: 300, imgUrl: null},
+    { id: 'bnsk53', title: 'Zorba the Greek', price: 87, imgUrl: null}
+]
+
+function getBooks() {
+    return gBooks
+}    
+
+function render() {}
